ce4edf7a86566dfac11504820d0b45923c50e345
Rommy Elyan <relyan@email.arizona.edu>
2018-04-18 18:11:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board()
 puts "   "+"|"+"   "+"|"+"   "+"\n"
-print "-----------"+"\n"
-print "   "+"|"+"   "+"|"+"   "+"\n"
-print "-----------"
+puts "-----------"+"\n"
+puts "   "+"|"+"   "+"|"+"   "+"\n"
+puts "-----------"
 end
